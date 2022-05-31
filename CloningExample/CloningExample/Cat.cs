6d045a168f400e3778d32b87ba18ddd9a822cca9
daviswcci <79462946+daviswcci@users.noreply.github.com>
2022-05-31 17:10:40 -0400

    cloning demo

---
+++ CloningExample/CloningExample/Cat.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloningExample
+{
+    internal class Cat : ICloneable, IComparable<Cat>, IEquatable<Cat>
+    {
+        public string Name;
+        public int Lives;
+        public bool AdditionalToes;
+        //public Owner Owner; // reference type. which means it is difficult to clone.
+
+        public Cat()
+        {
+            Name = "Dez";
+            Lives = 9;
+            AdditionalToes = false;
+        }
+
+        public Cat(string name, int lives, bool additionalToes)
+        {
+            Name = name;
+            Lives = lives;
+            AdditionalToes = additionalToes;
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine("Meow!");
+        }
+
+        public void Die()
+        {
+            Lives--;
+        }
+
+        public object Clone()
+        {
+            return new Cat(this.Name, this.Lives, this.AdditionalToes);
+        }
+
+        public int CompareTo(Cat? other)
+        {
+            // return 1 if this is greater than other
+            // return 0 if this is equal to other
+            // return -1 if this is less than other
+            if(this.Lives > other.Lives)
+            {
+                return 1;
+            }
+            else if (this.Lives == other.Lives)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public bool Equals(Cat? other)
+        {
+            return Lives == other.Lives && Name == other.Name;
+        }
+
+        public static bool operator ==(Cat left, Cat right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Cat left, Cat right)
+        {
+            return !left.Equals(right);
+        }
+    }
+}
---
+++ CloningExample/CloningExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using CloningExample;
+
+Cat Cat = new Cat();
+Console.WriteLine(Cat.Name);
+
+Cat anotherCat = Cat; // some might do this expecting to make a copy of Cat.
+Console.WriteLine(anotherCat.Name);
+Cat.Name = "Hammy"; // this should have no bearing on anotherCat... right??
+Console.WriteLine(anotherCat.Name); // Oh no! It says Hammy!
+
+
+
+Cat thirdCat = (Cat)Cat.Clone(); // making a copy of cat, completely separate from the original
+Console.WriteLine(thirdCat.Name);
+Cat.Name = "Pumpkin"; // this should have no bearing on thirdCat... right??
+Console.WriteLine(thirdCat.Name); // Hurray! No connection. We have a completely separate copy. 
+Console.WriteLine(thirdCat == Cat);
+
+
+Cat equalsCat = (Cat)Cat.Clone(); // created a clone of the Cat variable
+Console.WriteLine(equalsCat == Cat); // we expect this to be true
